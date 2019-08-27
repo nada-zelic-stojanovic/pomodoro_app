8f6a5e27d2fb3cfca4a18022b0d8982164d81988
27917297 <nada.zelic@gmail.com>
2019-08-27 14:54:07 +0200

    Play sound on countdown end

--- src/containers/Pomodoro/Pomodoro.js
+++ src/containers/Pomodoro/Pomodoro.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import RemainingTime from './../../components/RemainingTime';
 import styled from 'styled-components';
+import Sound from 'react-sound';
+import gongSound from './../../assets/gong.mp3';
 
 const TimerBox = styled.div`
   position: absolute;
@@ -10,16 +12,22 @@ const TimerBox = styled.div`
   text-align: center;
 `;
 
-const StartButton = styled.button``;
+const Button = styled.button`
+  width: 45px;
+  height: 45px;
+  border-radius: 50%;
+  border-style: none;
+`;
 
 class Pomodoro extends Component {
   state = {
     timerIsRunning: false,
-    seconds: 3
+    seconds: 5,
+    sessionEndSound: Sound.status.STOPPED
   };
 
   handleStartButton = () => {
-    this.setState({ timerIsRunning: true });
+    this.setState({ timerIsRunning: true, sessionEndSound: Sound.status.STOPPED });
     const timer = setInterval(() => {
       const { seconds } = this.state;
       this.setState({ seconds: seconds - 1 });
@@ -28,20 +36,20 @@ class Pomodoro extends Component {
     const { seconds } = this.state;
     const timeout = setTimeout(() => {
       clearInterval(timer);
-
-      this.setState({ timerIsRunning: false, seconds: seconds });
+      this.setState({ timerIsRunning: false, seconds: seconds, sessionEndSound: Sound.status.PLAYING });
     }, seconds * 1000);
   };
 
+
   render() {
-    const { seconds, timerIsRunning } = this.state;
+    const { seconds, timerIsRunning, sessionEndSound } = this.state;
     return (
       <TimerBox>
         <RemainingTime remainingSeconds={seconds} />
-        <StartButton onClick={this.handleStartButton} disabled={timerIsRunning}>
+        <Sound url={gongSound} playStatus={sessionEndSound}/>
+        <Button onClick={this.handleStartButton} disabled={timerIsRunning}>
           start
-        </StartButton>
-        >
+        </Button>
       </TimerBox>
     );
   }
