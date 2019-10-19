d32d0f20f148c4f58747abedeb467a0ed91ee022
Tsukuyomi212 <nada.zelic@gmail.com>
2019-10-19 12:20:14 +0200

    Show two-digit time using padStart()

--- src/components/RemainingTime.js
+++ src/components/RemainingTime.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Time, ClockBox } from '../uiComponents';
+import { convertSecondsToMinutes } from '../utils';
 
 const RemainingTime = ({ remainingSeconds }) => {
-  const minutes = Math.floor(remainingSeconds / 60);
-  const seconds = remainingSeconds % 60;
+  const time = convertSecondsToMinutes(remainingSeconds);
+  const { minutes, seconds } = time;
   return (
     <div>
       <ClockBox>
         <Time>
-          {minutes < 10 ? '0' + minutes : minutes} :{' '}
-          {seconds < 10 ? '0' + seconds : seconds}
+          {String(minutes).padStart(2, '0')} :{' '}
+          {String(seconds).padStart(2, '0')}
         </Time>
       </ClockBox>
     </div>
