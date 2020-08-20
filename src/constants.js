a63fdf59011b68984638f9f89d264b91b70554eb
Nada Zelic <tsukuyomi@Nadas-MacBook-Pro.local>
2020-08-20 19:35:31 +0200

    refactor

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const SESSION_LENGTH = 10 * 60;
-export const SHORT_BREAK_LENGTH = 2 * 60;
-export const LONG_BREAK_LENGTH = 10 * 60;
+export const SESSION_LENGTH = 25 * 60;
+export const SHORT_BREAK_LENGTH = 5 * 60;
+export const LONG_BREAK_LENGTH = 15 * 60;
